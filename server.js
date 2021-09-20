228f7b2c3694081341494442728badaad1fcab72
penggembala-domba25 <penggembala.domba025@gmail.com>
2021-09-20 11:36:40 +0700

    fixing typo

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8080
 
 app.use(express.static(path.join(__dirname, "build")))
 
-app.get("/", (res, req) => {
+app.get("/", (req, res) => {
     res.sendFile(
         path.resolve(__dirname, "build", "index.html")
     )
